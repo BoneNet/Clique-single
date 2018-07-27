2fe70543b64b103f2e5e2778814235a1d5d94748
TimSYQQX <tianyuxiong1997@gmail.com>
2018-07-28 03:35:14 +0800

    syntax fix

--- Clique-256-singlegpu/dataloader/data_generator.py
+++ Clique-256-singlegpu/dataloader/data_generator.py
@@ -1,30 +1,28 @@
 import sys
 import os
-#import urllib.request   ## python 3
-from urllib import urlretrieve ## python 2
+# import urllib.request   ## python 3
+from urllib import urlretrieve  ## python 2
 import tarfile
 import zipfile
 from preprocess import cifar_preprocess, svhn_preprocess, mura_preprocess
 import numpy as np
 
+
 def data_normalization(train_data_raw, test_data_raw, normalize_type):
-    if normalize_type=='divide-255':
-        train_data=train_data_raw/255.0
-        test_data=test_data_raw/255.0
-        
+    if normalize_type == 'divide-255':
+        train_data = train_data_raw / 255.0
+        test_data = test_data_raw / 255.0
+
         return train_data, test_data
 
-    elif normalize_type=='None':
-        
+    elif normalize_type == 'None':
+
         return train_data_raw, test_data_raw
-   
+
+
 def load_data(data_type, normalize_type):
-    data_type=="mura":
-    data_path="MURA-v1.1"
-    train_data_raw, train_labels, test_data_raw, test_labels=mura_preprocess(data_path)
-    normalize_type='divide-255'
-    train_data, test_data=data_normalization(train_data_raw, test_data_raw, normalize_type)
+    data_path = "MURA-v1.1"
+    train_data_raw, train_labels, test_data_raw, test_labels = mura_preprocess(data_path)
+    normalize_type = 'divide-255'
+    train_data, test_data = data_normalization(train_data_raw, test_data_raw, normalize_type)
     return train_data, train_labels, test_data, test_labels
-    
-
-
